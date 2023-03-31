4b29e9c1acbca6368ead672c86f8931083cd0754
Aleksey <apyusho@gmail.com>
2023-03-31 21:16:00 +0200

    Revert "CameraController is Finish"
    This reverts commit 65fff1af7219f997c461cd88d46173d856094cf1.

--- TowerDefence_Puisho_Zubchenko/Assets/Scripts/CameraController.cs
+++ TowerDefence_Puisho_Zubchenko/Assets/Scripts/CameraController.cs
@@ -5,82 +5,25 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private Transform _camera;
-    [SerializeField] private float _speedPosition;
-    [SerializeField] private float _speedZoom;
-
-    [SerializeField] private float _minX;
-    [SerializeField] private float _minZ;
-    [SerializeField] private float _maxX;
-    [SerializeField] private float _maxZ;
-
-    [SerializeField] private float _zoomDistance;
-    private const float _speedCorrectedHeight = 60f;
-
-    private Touch _touch0;
-    private Touch _touch1;
-
-    private Vector2 _centerScreen;
-    void Awake()
+    private float _speedCamera;
+    void Start()
     {
         _camera = gameObject.transform;
-        _centerScreen = new Vector2(Screen.width / 2, Screen.height / 2);
     }
 
-    void LateUpdate()
+    void Update()
     {
-        CameraRaycast();
-        CameraMovement(_camera.transform);
-        Zoom();
-        
+        CameraMovement(_camera);
     }
     private void CameraMovement(Transform camera)
     {
-        if (Input.touchCount == 1)
+        if(Input.touchCount > 0)
         {
-            var touchPoint = Input.GetTouch(0);
-            float speedX = touchPoint.deltaPosition.x * _speedPosition * Time.deltaTime;
-            float speedZ = touchPoint.deltaPosition.y * _speedPosition * Time.deltaTime;
-
-            camera.position = new Vector3(Mathf.Clamp(camera.position.x + speedX, _minX, _maxX), camera.position.y, Mathf.Clamp(camera.position.z + speedZ, _minZ, _maxZ));
-        }
-    }
-    private void Zoom()
-    {       
-        if(Input.touchCount == 2)
-        {
-            _touch0 = Input.GetTouch(0);
-            _touch1 = Input.GetTouch(1);
-            float delta = (Mathf.Abs(_touch0.deltaPosition.x) + Mathf.Abs(_touch1.deltaPosition.x)) * _speedZoom * Time.deltaTime;
-            if (_touch0.phase == TouchPhase.Moved && _touch1.phase == TouchPhase.Moved)
+            if(Input.touchCount == 1)
             {
-                if (_touch0.position.x < _centerScreen.x)
-                {
-                    if (_touch0.deltaPosition.x < 0)
-                        _zoomDistance -= delta;
-                    else
-                        _zoomDistance += delta;
-                }
-                else
-                {
-                    if (_touch0.deltaPosition.x < 0)
-                        _zoomDistance += delta;
-                    else
-                        _zoomDistance -= delta;
-                }
-            }
-        }
-    }
-    private void CameraRaycast()
-    {
-        if (Physics.Raycast(_camera.position, Vector3.down, out RaycastHit hit))
-        {
-            if (hit.distance < _zoomDistance - 1f)
-            {
-                _camera.position = new(_camera.position.x, _camera.position.y + _speedCorrectedHeight * Time.deltaTime, _camera.position.z);
-            }
-            else if (hit.distance > _zoomDistance + 1f)
-            {
-                _camera.position = new(_camera.position.x, _camera.position.y - _speedCorrectedHeight * Time.deltaTime, _camera.position.z);
+                var touchPoint = Input.GetTouch(0);
+                camera.position = new(camera.position.x + touchPoint.deltaPosition.x * Time.deltaTime, 
+                    camera.position.y, camera.position.z + touchPoint.deltaPosition.y * Time.deltaTime);
             }
         }
     }
